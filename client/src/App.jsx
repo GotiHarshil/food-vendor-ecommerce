71fdc6915303a318fa30065625e7d617987b015f
Harshil <harshilgoti01@gmail.com>
2025-10-31 19:06:21 -0400

    Added the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Navbar from "./navbar.jsx"; 
 
 function App() {
   const [foods, setFoods] = useState([]);
@@ -11,29 +12,10 @@ function App() {
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    
+    <div>
+      <Navbar />
       <h1>Food Vendor Menu</h1>
-
-      {foods.length === 0 ? (
-        <p>Loading menu...</p>
-      ) : (
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          {foods.map((item) => (
-            <li
-              key={item.id}
-              style={{
-                background: "#f4f4f4",
-                margin: "10px auto",
-                padding: "10px",
-                borderRadius: "8px",
-                width: "300px",
-              }}
-            >
-              🍴 <strong>{item.name}</strong> — ${item.price.toFixed(2)}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 }
