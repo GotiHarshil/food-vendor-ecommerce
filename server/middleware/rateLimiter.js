034bce7e41264df0f59b267e7260a47698f4a954
Harshil <harshilgoti01@gmail.com>
2026-05-17 00:09:21 -0400

    Security added

--- server/middleware/rateLimiter.js
+++ server/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const globalApiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 150,
+  max: 200,
   standardHeaders: true,
   legacyHeaders: false,
   message: { error: "Too many requests, please try again later." },
